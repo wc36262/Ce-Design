18d9c8e693e8db238d679b1c182c41ae2e8724ac
peterwang <peterwang36262@gmail.com>
2020-06-14 17:58:38 +0800

    Upload组件基本完成

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {FC} from 'react'
 import classNames from 'classnames'
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 
@@ -7,8 +7,8 @@ export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning
 export interface IconProps extends FontAwesomeIconProps {
     theme?: ThemeProps
 }
-
-const Icon: React.FC<IconProps> = (props) => {
+ 
+export const Icon: FC<IconProps> = (props) => {
     const { className, theme, ...restProps } = props
     const classes = classNames('ce-icon', className, {
         [`icon-${theme}`]: theme
